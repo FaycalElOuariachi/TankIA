74863cd7f821027ec8f6cbbda7b266965be94a41
FaycalElOuariachi <faycal.eloua@gmail.com>
2016-04-25 00:10:10 +0200

    changement de paramètre : mode record

--- Tank IA/Assets/Scripts/ScenesParameters/ScenesParameters.cs
+++ Tank IA/Assets/Scripts/ScenesParameters/ScenesParameters.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.IO;
 
 public class ScenesParameters {
-	static public bool m_HasRecorder = false;
+	static public bool m_HasRecorder = true;
 	static public int m_GameNumber = -1;
 	//static public string[] m_IATanks = {"IATankOne.dll", "IATankTwo.dll"};
 	static public string[] m_IATanks = {"", ""};
