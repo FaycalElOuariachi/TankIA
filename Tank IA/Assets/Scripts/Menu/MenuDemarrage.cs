96137f42bc162365dd4d685b37e0bacd3a73ba31
FaycalElOuariachi <faycal.eloua@gmail.com>
2016-05-10 22:50:56 +0200

    Gestion des IA à partir du menu

--- Tank IA/Assets/Scripts/Menu/MenuDemarrage.cs
+++ Tank IA/Assets/Scripts/Menu/MenuDemarrage.cs
@@ -12,8 +12,14 @@ public class MenuDemarrage : MonoBehaviour {
 
     public Dropdown choiceGame;
     public Dropdown choiceParty;
+	public Dropdown listeIAPlay1;
+	public Dropdown listeIAPlay2;
     private string m_PathRecorder = @"records";
+	private string m_PathIA = @"Assets"+Path.AltDirectorySeparatorChar+"Library"+Path.AltDirectorySeparatorChar+"IA";
     private string[] filesRecords;
+	private string[] filesIA;
+	private string fileNameIA;
+	private string[] ia_players = new string[2];
     private string recordName;
     private Dropdown[] listDropdown;
     private Toggle[] choiceUser;
@@ -55,7 +61,19 @@ public class MenuDemarrage : MonoBehaviour {
             {
                 choiceParty = listDropdown[i];
             }
+			if (listDropdown [i].name.Equals ("listeIA1")) {
+				listeIAPlay1 = listDropdown [i];
+			}
+			if (listDropdown [i].name.Equals ("listeIA2")) {
+				listeIAPlay2 = listDropdown [i];
+			}
         }
+
+		listeIAPlay1.enabled = false;
+		listeIAPlay1.enabled = false;
+
+
+		
 
        choiceUser = GetComponentsInChildren<Toggle>();
 
@@ -71,6 +89,9 @@ public class MenuDemarrage : MonoBehaviour {
             }
         }
         
+		player1Confirm.onValueChanged.AddListener (checkedIA1);
+
+		player2Confirm.onValueChanged.AddListener (checkedIA2);
        
         //recherche des éléments UI
         listReplayText = GameObject.Find("Replays");
@@ -82,6 +103,8 @@ public class MenuDemarrage : MonoBehaviour {
         player1Text.transform.localPosition = new Vector3(-75, 90, 0);
         player2Text.transform.localPosition = new Vector3(-75, 50, 0);
         player1Confirm.transform.localPosition = new Vector3(-5, 95, 0);
+		listeIAPlay1.transform.localPosition = new Vector3 (75, 95, 0);
+		listeIAPlay2.transform.localPosition = new Vector3 (75, 50, 0);
         player2Confirm.transform.localPosition = new Vector3(-5, 55, 0);
         listReplayText.transform.localPosition = new Vector3(-75, 90, 0);
         listParties.transform.localPosition = new Vector3(-55, 45, 0);
@@ -93,6 +116,8 @@ public class MenuDemarrage : MonoBehaviour {
         listParties.gameObject.SetActive(false);
         player1Text.gameObject.SetActive(false);
         player2Text.gameObject.SetActive(false);
+		listeIAPlay1.gameObject.SetActive (false);
+		listeIAPlay2.gameObject.SetActive (false);
 
 
         //effacer les options de la liste du choix du jeu
@@ -114,11 +139,65 @@ public class MenuDemarrage : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         
+		if (player1Confirm.isOn) {
+			listeIAPlay1.enabled = true;
 
+		} else {
+			listeIAPlay1.enabled = false;
+		}
+		if (player2Confirm.isOn) {
+			listeIAPlay2.enabled = true;
+		} else {
+			listeIAPlay2.enabled = false;
+		}
     }
 
+	public void checkedIA1(bool value){
+		
+		if (value) {
+			populateListIA (1);
+		} else {
+			listeIAPlay1.ClearOptions ();
+		}
 
-    
+	}
+
+	public void checkedIA2(bool value){
+		if (value) {
+			populateListIA (2);
+		} else {
+			listeIAPlay2.ClearOptions ();
+		}
+
+	}
+
+
+	public void populateListIA(int numero){
+		//peuple la liste des IA
+		filesIA = Directory.GetFiles(m_PathIA);
+		for (int i = 0; i < filesIA.Length; i++)
+		{
+
+			if (filesIA [i].EndsWith (".dll")) {
+				fileNameIA = filesIA [i].Substring (18);
+				Dropdown.OptionData iaOption = new Dropdown.OptionData (fileNameIA);
+				if (numero == 1) {
+					if (!listeIAPlay1.options.Contains (iaOption)) {
+						listeIAPlay1.options.Add (iaOption);
+					}
+					listeIAPlay1.value = 0;
+				}
+				if (numero == 2) {
+					if (!listeIAPlay2.options.Contains (iaOption)) {
+						listeIAPlay2.options.Add (iaOption);
+					}
+				}
+				listeIAPlay2.value = 0;
+			}
+
+
+		}
+	}
 
 
 
@@ -141,6 +220,7 @@ public class MenuDemarrage : MonoBehaviour {
    
     public void populateList()
     {
+		//peuple la liste des records
         filesRecords = Directory.GetDirectories(m_PathRecorder);
         for (int i = 0; i < filesRecords.Length; i++)
         {
@@ -150,23 +230,41 @@ public class MenuDemarrage : MonoBehaviour {
             choiceGame.options.Add(recordOption);
             choiceGame.value = 0;
         }
+
+
+
     }
 
    
     //méthode pour lancer le jeu lors du clique
-    public void startGame()
+	public void startGame()
     {
 
         isPlayer1Static = player1Confirm.isOn;
         isPlayer2Static = player2Confirm.isOn;
+		if (!player1Confirm.isOn) {
+			ia_players [0] = "";
+		} else {
+			ia_players [0] = listeIAPlay1.captionText.text;
+		}
+
+		if (!player2Confirm.isOn) {
+			ia_players [1] = "";
+		} else {
+			ia_players [1] = listeIAPlay2.captionText.text;
+		}
+
+		ScenesParameters.m_IATanks [0] = ia_players [0];
+		ScenesParameters.m_IATanks [1] = ia_players [1];
+		Debug.Log ("afficherok"+ScenesParameters.m_IATanks [0]);
 		//Application.LoadLevel(1);
 		SceneManager.LoadScene(1);
     }
 
-	public void replayGame( string nameReplay)//string nameReplay)
+	public void replayGame( int numeroReplay)//string nameReplay)
     {
-		ScenesParameters.m_GameName = @"records" + Path.AltDirectorySeparatorChar + nameReplay;
-		Debug.Log (ScenesParameters.m_GameName);
+
+		ScenesParameters.m_GameNumber = numeroReplay + 1;
 		//Debug.Log (numeroReplay + 1);//.Substring (4));
 		//Application.LoadLevel(1);
 		SceneManager.LoadScene(1);
@@ -198,6 +296,8 @@ public class MenuDemarrage : MonoBehaviour {
                     player1Confirm.gameObject.SetActive(true);
                     player2Confirm.gameObject.SetActive(true);
                     player2Text.gameObject.SetActive(true);
+					listeIAPlay1.gameObject.SetActive (true);
+					listeIAPlay2.gameObject.SetActive (true);
 
 
 
@@ -236,7 +336,8 @@ public class MenuDemarrage : MonoBehaviour {
 
             if (GUI.Button(new Rect(Screen.width / 2 + 100, Screen.height / 2, 160, 30), "Start Replay"))
             {
-				replayGame ( choiceGame.captionText.text);//choiceGame.value ); //choiceGame.itemText.text);
+
+				replayGame ( choiceGame.value ); //choiceGame.itemText.text);
 
             }
 
@@ -248,6 +349,8 @@ public class MenuDemarrage : MonoBehaviour {
                 choiceParty.gameObject.SetActive(false);
                 listReplayText.gameObject.SetActive(false);
                 listParties.gameObject.SetActive(false);
+				listeIAPlay1.gameObject.SetActive (false);
+				listeIAPlay2.gameObject.SetActive (false);
 
 
             }
@@ -272,6 +375,8 @@ public class MenuDemarrage : MonoBehaviour {
                     player1Confirm.gameObject.SetActive(false);
                     player2Confirm.gameObject.SetActive(false);
                     player2Text.gameObject.SetActive(false);
+					listeIAPlay1.gameObject.SetActive (false);
+					listeIAPlay2.gameObject.SetActive (false);
 
 
 
